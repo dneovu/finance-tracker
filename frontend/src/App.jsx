8046c7042bdabf197ad4c9372c971e3fa4e584ac
dneovu <113314302+dneovu@users.noreply.github.com>
2025-01-30 15:05:29 +0300

    feat: move to ts

--- frontend/src/App.jsx
+++
@@ -1,5 +0,0 @@
-export const App = () => (
-  <>
-    <h1 className="text-blue-400">Hello, World!</h1>
-  </>
-);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,3 @@
+export const App = () => {
+  return <h1 className="bg-red-50 underline">Hello</h1>;
+};
